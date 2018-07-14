cbecd07238b8d6b3dadae6be2fd78d6178da4fe1
Svetloslav15 <sn_novoselski@abv.bg>
2018-07-14 10:44:17 +0300

    add exercise

--- 06. jQuery Exercise/02. Timer/timer.js
+++ 06. jQuery Exercise/02. Timer/timer.js
@@ -1,3 +1,37 @@
 function timer() {
-   // TODO:
+    $('#start-timer').on("click", startTimer);
+    $('#stop-timer').on("click", stopTimer);
+
+    let interval;
+    let secs = 1;
+
+    function startTimer() {
+        if (interval) {
+            clearInterval(interval);
+        }
+        interval = setInterval(update, 1000);
+    }
+
+    function update() {
+        let currentSec = Math.floor(secs % 60);
+        let currentMin = Math.floor(secs / 60 % 60);
+        let currentHours = Math.floor(secs / 3600);
+        if (currentHours < 10){
+            currentHours = "0" + currentHours;
+        }
+        if (currentMin < 10){
+            currentMin = "0" + currentMin;
+        }
+        if (currentSec < 10){
+            currentSec = "0" + currentSec;
+        }
+        $('#hours').text(currentHours);
+        $('#minutes').text(currentMin);
+        $('#seconds').text(currentSec);
+        secs++;
+    }
+
+    function stopTimer() {
+        clearInterval(interval);
+    }
 }
--- 06. jQuery Exercise/03. Form-Validation/form-validation.js
+++ 06. jQuery Exercise/03. Form-Validation/form-validation.js
@@ -1,3 +1,11 @@
 function validate() {
-	// TODO:
+	let userName = $('#username');
+	let email = $('#email');
+	let password = $('#password');
+	let conPass = $('#confirm-password');
+	let company = $('#company');
+	let comNum = $('#companyNumber');
+	$('#submit').on("click", function () {
+
+    });
 }
